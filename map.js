04f7c1452757d5568bbd72f027ddf97e142e0083
Leoncodes00 <chachaiyang3@gmail.com>
2019-11-13 16:11:01 -0800

    Added another demo marker to find distance

--- map.js
+++ map.js
@@ -1,4 +1,6 @@
 const ADDRESS_ARRAY = [];
+let MARKERA;
+let MARKERB;
 let POINTS = 5000;
 let LATA;
 let LNGA;
@@ -32,15 +34,26 @@ function initMap(){
             placeMarker(event.latLng);
     });
 }
-let marker;
 function placeMarker(location) {
-    if(marker){
-        marker.setPosition(location);
+    if(MARKERA){
+        MARKERA.setPosition(location);
     } else {
-        marker = new google.maps.Marker({
+        MARKERA = new google.maps.Marker({
             position: location, 
             map: map
         });
+    }
+
+    if(MARKERB){
+        MARKERB.setPosition(MARKERB.position);
+    } else {
+        MARKERB = new google.maps.Marker({
+            position: {
+                lat: 4.7,
+                lng: -74
+            },
+            map: map
+        })
     }
 }
 
@@ -94,10 +107,31 @@ function compareCoordinates(){
     let answer = document.createElement('p');
     answer.textContent = "Your guess is in " + ADDRESS_ARRAY[ADDRESS_ARRAY.length - 1];
     answerDiv.appendChild(answer);
+    console.log(MARKERA.position.lat(), MARKERA.position.lng());
+    console.log(MARKERB.position.lat(), MARKERB.position.lng());
 
     //Get the lat and lng from the random image and the user input
     //Find the distance between both of them
-    console.log(LATA, LNGA);
+    // Obtain the distance in meters by the computeDistanceBetween method
+    // From the Google Maps extension
+    
+    // function calcDistance () {
+    //     return google.maps.geometry.spherical.computeDistanceBetweeen(latlng1,latlng2)
+    // }
+    // calcDistance();
+    
+    // let distanceInMeters = geometryObject.computeDistanceBetween({
+    //     from: latlng1,
+    //     to: latlng2
+    // });
+
+    //console.log(distanceInMeters)
+
+// Outputs: Distance in Meters:  286562.7470149898
+//console.log("Distance in Meters: ", distanceInMeters);
+
+// Outputs: Distance in Kilometers:  286.5627470149898
+//console.log("Distance in Kilometers: ", (distanceInMeters * 0.001));
     //There is a maximum of 5000 points
     //The maximum points gets halved if the distance between the two points is 500
 
